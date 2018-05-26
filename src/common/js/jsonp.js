45bd0c12ce60b88b8eb297dfafe1cb4c42e51ef6
Leon <wanliangwang@163.com>
2018-05-26 23:05:35 +0800

    实现jsonp封装

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,24 @@
+import originJSONP from 'jsonp'
+
+export default function jsonp(url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+  return new Promise((resolve, reject) => {
+    originJSONP('url', option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+function param(data) {
+  let urlData = ''
+  for (var key in data) {
+    let value = data[key] === undefined ? '' : data[key]
+    urlData += `&${key}=${encodeURIComponent(value)}`
+  }
+  // Array.splice.apply(urlData, 0, 1)
+  return urlData ? urlData.substring(1) : ''
+}
--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="m-header">
     <div class="icon"></div>
-    <h1 class="text">HaHa Music</h1>
+    <h1 class="text">Brown Music</h1>
   </div>
 </template>
 
